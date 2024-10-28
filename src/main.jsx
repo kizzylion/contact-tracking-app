1fd1a49f1d7a85464ef21c4b26d0fb931654cbde
kizzylion <kztchm@gmail.com>
2024-10-28 06:31:34 -0400

    FEAT: CREATE CONTACT ROUTE UI

--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,16 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Root from "./routes/root";
-import "./index.css";
 import ErrorPage from "./error-page";
+import Contact from "./routes/contact";
+import "./index.css";
 
 const routes = createBrowserRouter([
   {
     path: "/",
     element: <Root />,
     errorElement: <ErrorPage />,
+    children: [{ path: "contacts/:contactId", element: <Contact /> }],
   },
 ]);
 
